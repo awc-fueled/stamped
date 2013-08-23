44de4c14a77db057f3cbc69e60365565d6d1009e
agconti <agc11d@gmail.com>
2013-08-23 10:12:30 -0400

    basic formatting additions to results and home feed

--- stamped_project/stamped/static/js/home_jquery.js
+++ stamped_project/stamped/static/js/home_jquery.js
@@ -1,11 +1,18 @@
-function get_map_place(output_list){
-	$("#output").append("<p>"+output_list[0]+"</p>")
-}
 $(document).ready(function(){
 	//initialize main map
 	$(google.maps.event.addDomListener(window, 'load', initialize));
 
-	//get values from google map when changed
+	$("#top_choice_list").selectable();
+	//or . hover
+	$("#top_choice_div").mouseEnter(function(){
+		$("#choice_cat_name").animate(function(){
+			fontSize: '20px'
+		});
+		//get children of cat list eventhough cat name is one? lets see what happens
+		$("#choice_cat_list li").animate(function(){
+			fontSize: '14px'
+		});
+	});
 
 
 });
--- stamped_project/stamped/views.py
+++ stamped_project/stamped/views.py
@@ -10,19 +10,10 @@ from django.core.serializers.json import DjangoJSONEncoder
 
 from stamped.models import Restaurant
 
+
+
 def home(request):
-	return render(request, "stamped/home.html")
-
-def results(request):
-	# import sys
-	# sys.stdout.write("\nresults view action\n")
-	# sys.stdout.write(result){'result':result}
-	restaurant = get_object_or_404(Restaurant, name='Fish', address='280 Bleecker St')
-	return render(request, "stamped/restaurant.html", {'restaurant': restaurant})
-
-def feed_home(request):
 	category_choices = [
-					('asian', 'Asian'),
 					('american', 'American'),
 					('bar_food', 'Bar Food'),
 					('seafood', 'Seafood'),
@@ -40,4 +31,12 @@ def feed_home(request):
 		top_5 = Restaurant.objects.filter(category=category[0]).order_by('rating')[:5]
 		top_choices.append(top_5)
 	import sys; sys.stdout.write(str(len(top_choices)))	
-	return render(request, "stamped/feed.html", {'top_choices': top_choices} )
+	return render(request, "stamped/home.html", {'top_choices': top_choices} )
+	
+def results(request):
+	# import sys
+	# sys.stdout.write("\nresults view action\n")
+	# sys.stdout.write(result){'result':result}
+	restaurant = get_object_or_404(Restaurant, name='Fish', address='280 Bleecker St')
+	return render(request, "stamped/restaurant.html", {'restaurant': restaurant})
+
