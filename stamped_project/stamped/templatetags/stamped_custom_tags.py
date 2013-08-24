cd4dea5af00e8c1626773c948178b59fda1b8236
agconti <agc11d@gmail.com>
2013-08-24 18:05:07 -0400

    advanced timeposted tag functionality

--- stamped_project/stamped/templatetags/stamped_custom_tags.py
+++ stamped_project/stamped/templatetags/stamped_custom_tags.py
@@ -10,34 +10,20 @@ def do_time_since(parser, token):
         tag_name, date_to_format = token.split_contents()
     except ValueError:
         raise template.TemplateSyntaxError("%r tag requires a single argument" % token.contents.split()[0])
-    return TimeSinceNode(date_to_format)
+    return TimePostedNode(date_to_format)
 
-class TimeSinceNode(template.Node):
+class TimePostedNode(template.Node):
     def __init__(self, date_to_format):
         self.date_to_format = template.Variable(date_to_format)
     def render(self, context):
         try:
             d = self.date_to_format.resolve(context)
             delta = datetime.datetime.now() - d
-            return "Posted %s days %s minutes and %s seconds ago" %(delta.days, delta.seconds//3600, (delta.seconds//60)%60)
+            if delta.days > 7:
+                return "Posted %s days ago." %(delta.days)
+            elif delta.seconds//3600 > 1:
+                return "Posted %s days and %s minutes ago." %(delta.days, delta.seconds//3600)
+            else:
+                 return "Posted %s seconds ago." %(delta.seconds//60)%60
         except template.VariableDoesNotExist:
             return ''
- 
-
-
-# @register.tag(name="timeSince")
-# def do_time_since(parser,token):
-#     nodelist = parser.parse(('endupper',))
-#     parser.delete_first_token()
-    
-#     return UpperNode(nodelist)
-    
-# class UpperNode(template.Node):
-#     def __init__(self,nodelist):
-#         self.nodelist = nodelist
-        
-#     def render(self,context):
-#         output = self.nodelist.render(context)
-        
-#         return output.upper()
-        
